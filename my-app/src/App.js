4fbc8194e7a942079799e0cd1106534371adef06
Syd Rigel <prest261@umn.edu>
2020-04-12 15:38:15 -0600

    set up navbar and basic components and added info to the about page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-// import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from "./components/NavBar";
+import About from "./pages/About";
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+
+// NavBar on top => used to click between "pages"
+// /game => Game 
+// /login => Login
+// /score => Score
+// ANYWHERE ELSE => Home
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <div className="container">
+        <Switch>
+          {/* you can only match ONE route inside */}
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          {/* take home for anything else */}
+          <Route component={About} />
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
 export default App;
+
---
+++ my-app/src/pages/About.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const About = () => {
+    return (
+        // remember => a component(function) can only return ONE thing!!
+        <>
+            <h2>About Me</h2>
+            <p class="wrap-text">My name is Sydney Preston and I am from Olympia Washington, but I’ve lived in
+            several other states throughout my life.
+            I was raised by my mother and in sixth grade we made our first big move to the outskirts of Chicago.
+            We stayed in Chicago for a year and a half before moving to Raleigh, North Carolina.
+            After graduating high school, I got accepted to the University of Minnesota and moved to
+                    Minneapolis; where I studied photography and graduated in 2016 with a fine arts degree.</p>
+
+
+            <p class="wrap-text">In 2017 I decided to make a big life change and move to Colorado. Before I moved, I
+            was working as a design engineer at a printing company.
+            It wasn’t the job for me, so I decided to take a step back and figure out what I wanted to do.
+            I began working at Starbucks and I have since worked my way up from Barista to Shift Supervisor.
+            I still work at Starbucks, but I have moved my focus to learning web development so that I can make
+            a career change.
+                    Hopefully by next summer I can begin a new career in the tech industry.</p>
+
+
+            <p class="wrap-text">Since moving to Colorado, I have experienced a lot of growth in my personal life as
+            well as finding a career path I’d like to pursue.
+            After moving I began my journey of aerial dance and aerial performance.
+            I found a studio that has become such an important part of my life and I can’t imagine life without
+            all my aerial friends.
+            In my free time I enjoy aerial dance, performing, snowboarding, hiking, listening to live music, and
+            enjoying time with friends.
+            I am excited to continue pursuing aerial dance in my freetime as well as to see where this new
+                    career path will take me! </p>
+
+        </>
+    )
+
+};
+
+export default About;
