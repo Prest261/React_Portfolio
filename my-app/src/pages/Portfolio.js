6fb322808fdf3822ef15e2dc61088bec6d5c7cfa
Syd Rigel <prest261@umn.edu>
2020-04-12 16:52:30 -0600

    added the cards to the portfolio page

--- my-app/src/pages/Portfolio.js
+++ my-app/src/pages/Portfolio.js
@@ -1,8 +1,42 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
 
 const Portfolio = () => {
     return (
-        <h2>Portfolio!!!</h2>
+        <div id="portfolioContainer">
+
+            <CardDeck>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Body>
+                        <Card.Link href="#">Card Link</Card.Link>
+                        <Card.Link href="#">Another Link</Card.Link>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Body>
+                        <Card.Link href="#">Card Link</Card.Link>
+                        <Card.Link href="#">Another Link</Card.Link>
+                    </Card.Body>
+                </Card>
+            </CardDeck>
+        </div>
     )
 };
 
