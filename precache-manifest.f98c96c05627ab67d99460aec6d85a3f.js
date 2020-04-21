a0218deaca6b68eb4a64778595ca8fcba44bdefa
Syd Rigel <prest261@umn.edu>
2020-04-20 18:13:15 -0600

    Updates

---
+++ precache-manifest.f98c96c05627ab67d99460aec6d85a3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77be538390a96e87f7993e70d6b0de38",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "59cbd0af11f8fecd6f16",
+    "url": "/React_Portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6f3b65240600addf5e42",
+    "url": "/React_Portfolio/static/css/main.00cd74e4.chunk.css"
+  },
+  {
+    "revision": "59cbd0af11f8fecd6f16",
+    "url": "/React_Portfolio/static/js/2.ce83c7ab.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/React_Portfolio/static/js/2.ce83c7ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f3b65240600addf5e42",
+    "url": "/React_Portfolio/static/js/main.ab3f9df7.chunk.js"
+  },
+  {
+    "revision": "18dcad6835e4ee0a1aa9",
+    "url": "/React_Portfolio/static/js/runtime-main.75454567.js"
+  },
+  {
+    "revision": "636b177e833772487fd5081ba66d93d0",
+    "url": "/React_Portfolio/static/media/pink-triangles.636b177e.jpg"
+  }
+]);
